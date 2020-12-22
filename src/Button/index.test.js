bbb83eb4db44c4ab3a0e56bc59db034f174c0faf
Paweł Walczewski <paulwalczewski@icloud.com>
2020-12-22 17:08:45 +0100

    Add react testing utils config

--- src/Button/index.test.js
+++ src/Button/index.test.js
@@ -1,7 +1,12 @@
+import React from 'react'
 import Button from '.'
+
+import { render } from '@testing-library/react'
 
 describe('Button', () => {
   it('is truthy', () => {
-    expect(Button).toBeTruthy()
+    const { queryByText } = render(<Button message='test' />)
+
+    expect(queryByText('test')).toBeTruthy()
   })
 })
